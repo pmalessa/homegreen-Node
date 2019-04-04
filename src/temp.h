d04d9b19ca4b08badf54680f8e8d28ce3376986c
Phil Malessa <phil.malessa@tuhh.de>
2019-04-04 14:50:35 +0200

    removing software based I2C Master

--- src/temp.h
+++ src/temp.h
@@ -8,6 +8,20 @@
 #ifndef TEMP_H_
 #define TEMP_H_
 
+#define LM75A_DEFAULT_ADDRESS		0x48		// Address is configured with pins A0-A2
+#define LM75A_REGISTER_TEMP			0			// Temperature register (read-only)
+#define LM75A_REGISTER_CONFIG		1			// Configuration register
+#define LM75A_REGISTER_THYST		2			// Hysterisis register
+#define LM75A_REGISTER_TOS			3			// OS register
+#define LM75A_REGISTER_PRODID		7			// Product ID register - Only valid for Texas Instruments
+
+#define LM75_CONF_OS_COMP_INT		1			// OS operation mode selection
+#define LM75_CONF_OS_POL			2			// OS polarity selection
+#define LM75_CONF_OS_F_QUE			3			// OS fault queue programming
+
+#define LM75A_INVALID_TEMPERATURE	-1000.0f	// Just an arbritary value outside of the sensor limits
+
+
 void temp_init();
 float temp_getTemperature();
 
